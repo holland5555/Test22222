beb3c0e04c0e39992a92414c6c0f15c3f8ceed6f
holland044 <chuck@blubracket.com>
2021-08-02 13:28:58 -0700

    Update Infracode3.tf

--- Infracode3.tf
+++ Infracode3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "my-tf-test-bucket"
-  acl    = "public-read"
+  acl    = "private"
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
